1c7f3cfcc4387d1a4b9f80004d30364d8f2cbdc3
Matheus-Fontinele-Alves-Vieria <matheusfont.123@gmail.com>
2022-11-30 16:21:16 -0300

    UPDATE geral structure

--- src/get_data/get_data.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-class GetData(object):
-    def __init__(self):
-        pass
-    
-    def open_csv(self):
-        return pd.read_csv('../data/Air-pollution/death-rates-total-air-pollution.csv')
